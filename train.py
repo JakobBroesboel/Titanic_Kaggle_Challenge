03d2141680c425de76159e28e094a70ca0f530a9
ChristianEschen <jb1_91@msn.com>
2018-09-30 19:32:25 +0200

    Its alive

--- train.py
+++ train.py
@@ -4,8 +4,8 @@ import pandas as pd
 np.random.seed(1)
 
 def nonlin(x, deriv=False):
-    if(deriv):
-        return x * (x - 1)
+    if(deriv == True):
+        return x * (1 - x)
     return 1 / (1 + np.exp(-x))
 
 def init_weights(*layer):
@@ -14,7 +14,7 @@ def init_weights(*layer):
 		result = result + [2 * np.random.rand(layer[i],layer[i+1]) - 1]
 	return result
 
-def forward_prop(input, weights):
+def forward_prop(weights, input):
     currentActivations = [input]
     for synapses in weights:
         currentActivations = currentActivations + [nonlin(np.dot(currentActivations[-1],synapses))]
@@ -32,34 +32,65 @@ def backward_prop(activations, weights, output):
     return deltas[::-1], np.mean(np.abs(predicterror))
 
 def update_weights(activations, weights, deltas, lrate):
-    for synapses in range(len(deltas))[:1]:
+    for synapses in range(len(deltas)):
         weights[synapses] += activations[synapses].T.dot(deltas[synapses]) * lrate
-    return weights
 
-def predict(input, weights):
-    return forward_prop(input, output, weights)[-1]
+def predict(pred):
+    if(pred < 0.5):
+        return 0
+    return 1
 
+def train(weights, input, output, lrate, epochs):
+    for x in range(epochs):
+        activations = forward_prop(weights, input)
+        deltas, error = backward_prop(activations, weights, output)
+        if (x% 10000) == 0:
+            print "Epoch: " + str(x) + " | Error: " + str(error)
+        update_weights(activations, weights, deltas, lrate)
+
+def test(weights, input, output):
+    predictions = forward_prop(weights, input)[-1]
+    return float([v[0] == predict(v[1]) for v in zip(output, predictions)].count(True)) / len(output)
+
+
+def kfoldcrossval(input, output, k, lrate, epochs, layers):
+    folds_input = np.array_split(input, k)
+    folds_output = np.array_split(output, k)
+    errors = []
+
+    for i in range(k):
+        weights = init_weights(*layers)
+
+        test_input = folds_input[i]
+        test_output = folds_output[i]
+
+        train_input = np.concatenate(np.delete(folds_input, i, 0))
+        train_output = np.concatenate(np.delete(folds_output, i, 0))
+
+        train(weights, train_input, train_output, lrate, epochs)
+        result = test(weights, test_input, test_output)
+        errors += [result]
+        print("fold: " + str(i) + ", success rate: " + str(result))
+
+    return errors
 
 df =  pd.read_csv("data/processed_train.csv")
 
 X = df.values[:,1:]
 Y = df['Survived'].values[np.newaxis].T
 
-weights = init_weights(len(X[0]),5,6,9,1)
-
 input = X
 output = Y
+"""
+input = np.array([[0,0,1],
+            [0,1,1],
+            [1,0,1],
+            [1,1,1]])
 
-
-
-for x in range(60000):
-    activations = forward_prop(input, weights)
-    deltas, error = backward_prop(activations, weights, output)
-    if (x% 10000) == 0:
-        print "Error:" + str(error)
-    update_weights(activations, weights, deltas, 0.01)
-
-total = 714
-true = 0
-
-for x in range(len(input)):
+output = np.array([[0],
+			[1],
+			[1],
+			[0]])
+"""
+layers = [len(input[0]), 12, len(output[0])]
+kfoldcrossval(input, output, 2, 0.01, 60000, layers)
